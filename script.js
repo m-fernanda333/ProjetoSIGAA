3ebd8c652b54bd6070c77ef8fedfdb35345d96e1
M. Fernanda Pinheiro <73649581+m-fernanda333@users.noreply.github.com>
2022-04-24 17:49:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+/* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+Data De Modificação:  24/04;
+Autor:  M. Fernanda Pinheiro - mfpinheiro02@gmail.com;
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+
+
+function openPopUp() { //mostrar menu popup no display
+	document.getElementById('menuPopUp').style.display = 'block'; //faz aparecer o menu pop/up
+}
+
+function closePopUp() {//ocultar menu popup no display
+	document.getElementById('menuPopUp').style.display = 'none';
+}
