3ca339a87c1db0427dabafd1bbd2a327cf876c46
Ruan <ruanalmeidaifet@gmail.com>
2022-04-25 23:38:27 -0300

    Remoção de links de pagina / Mudança de sistema realizada apenas com javascript

--- script.js
+++ script.js
@@ -6,6 +6,67 @@ Autor:  M. Fernanda Pinheiro - mfpinheiro02@gmail.com;
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 
 
+function changePage1(){
+	document.getElementById('Titulo').innerHTML = 'SIGAA';
+	document.getElementById('item1').className = 'ItemNavigationBarActive';
+	document.getElementById('item2').className = 'ItemNavigationBar';
+	document.getElementById('item3').className = 'ItemNavigationBar';
+	document.getElementById('item4').className = 'ItemNavigationBar';
+	document.getElementById('item5').className = 'ItemNavigationBar';
+	document.getElementById('item6').className = 'ItemNavigationBar';
+};
+
+function changePage2(){
+	document.getElementById('Titulo').innerHTML = 'SIPAC';
+	document.getElementById('item1').className = 'ItemNavigationBar';
+	document.getElementById('item2').className = 'ItemNavigationBarActive';
+	document.getElementById('item3').className = 'ItemNavigationBar';
+	document.getElementById('item4').className = 'ItemNavigationBar';
+	document.getElementById('item5').className = 'ItemNavigationBar';
+	document.getElementById('item6').className = 'ItemNavigationBar';
+};
+
+function changePage3(){
+	document.getElementById('Titulo').innerHTML = 'SIGRH';
+	document.getElementById('item1').className = 'ItemNavigationBar';
+	document.getElementById('item2').className = 'ItemNavigationBar';
+	document.getElementById('item3').className = 'ItemNavigationBarActive';
+	document.getElementById('item4').className = 'ItemNavigationBar';
+	document.getElementById('item5').className = 'ItemNavigationBar';
+	document.getElementById('item6').className = 'ItemNavigationBar';
+};
+
+function changePage4(){
+	document.getElementById('Titulo').innerHTML = 'SIGPP';
+	document.getElementById('item1').className = 'ItemNavigationBar';
+	document.getElementById('item2').className = 'ItemNavigationBar';
+	document.getElementById('item3').className = 'ItemNavigationBar';
+	document.getElementById('item4').className = 'ItemNavigationBarActive';
+	document.getElementById('item5').className = 'ItemNavigationBar';
+	document.getElementById('item6').className = 'ItemNavigationBar';
+};
+
+function changePage5(){
+	document.getElementById('Titulo').innerHTML = 'SIGED';
+	document.getElementById('item1').className = 'ItemNavigationBar';
+	document.getElementById('item2').className = 'ItemNavigationBar';
+	document.getElementById('item3').className = 'ItemNavigationBar';
+	document.getElementById('item4').className = 'ItemNavigationBar';
+	document.getElementById('item5').className = 'ItemNavigationBarActive';
+	document.getElementById('item6').className = 'ItemNavigationBar';
+};
+
+function changePage6(){
+	document.getElementById('Titulo').innerHTML = 'SIGAdmin';
+	document.getElementById('item1').className = 'ItemNavigationBar';
+	document.getElementById('item2').className = 'ItemNavigationBar';
+	document.getElementById('item3').className = 'ItemNavigationBar';
+	document.getElementById('item4').className = 'ItemNavigationBar';
+	document.getElementById('item5').className = 'ItemNavigationBar';
+	document.getElementById('item6').className = 'ItemNavigationBarActive';
+};
+
+
 function openPopUp() { //mostrar menu popup no display
 	document.getElementById('menuPopUp').style.display = 'block'; //faz aparecer o menu pop/up
 }
